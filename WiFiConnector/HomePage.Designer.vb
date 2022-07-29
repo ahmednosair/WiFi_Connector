0c06430d64160fab1c917c197ac86e52b1b63df5
Nosair <ahmednoser1@gmail.com>
2022-07-30 00:21:56 +0200

    initial version

---
+++ WiFiConnector/HomePage.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class HomePage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NetworksList = New System.Windows.Forms.TableLayoutPanel()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(46, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(275, 44)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Scan available networks"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(51, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(270, 39)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "WiFi Connector"
+        '
+        'NetworksList
+        '
+        Me.NetworksList.AutoScroll = True
+        Me.NetworksList.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset
+        Me.NetworksList.ColumnCount = 1
+        Me.NetworksList.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.NetworksList.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NetworksList.Location = New System.Drawing.Point(46, 151)
+        Me.NetworksList.Name = "NetworksList"
+        Me.NetworksList.RowCount = 1
+        Me.NetworksList.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.NetworksList.Size = New System.Drawing.Size(275, 473)
+        Me.NetworksList.TabIndex = 2
+        '
+        'HomePage
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(363, 669)
+        Me.Controls.Add(Me.NetworksList)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "HomePage"
+        Me.Text = "WiFi Connector"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NetworksList As TableLayoutPanel
+End Class
---
+++ WiFiConnector/HomePage.vb
@@ -0,0 +1,113 @@
+﻿Imports Windows.Devices.WiFi
+Imports Windows.Security.Credentials
+
+
+Public Class HomePage
+    Dim adapter As WiFiAdapter
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim result = Await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(WiFiAdapter.GetDeviceSelector())
+        adapter = Await WiFiAdapter.FromIdAsync(result.ElementAt(0).Id)
+        Await adapter.ScanAsync()
+        Dim report = adapter.NetworkReport
+        NetworksList.Controls.Clear()
+        For Each network In report.AvailableNetworks
+            Dim networkView As TableLayoutPanel = New TableLayoutPanel
+            networkView.AutoSize = True
+            networkView.AutoSizeMode = AutoSizeMode.GrowAndShrink
+            Dim ssid As New Label
+
+            ssid.AutoSize = True
+            ssid.Text = "Ssid: " & If(network.Ssid.Length = 0, "Hidden Network", network.Ssid)
+            networkView.Controls.Add(ssid)
+            Dim bssid As New Label
+            bssid.AutoSize = True
+            bssid.Text = "Bssid: " & network.Bssid
+            networkView.Controls.Add(bssid)
+            Dim rssi As New Label
+            rssi.AutoSize = True
+            rssi.Text = "Rssi: " & network.NetworkRssiInDecibelMilliwatts & " dBm"
+            networkView.Controls.Add(rssi)
+            Dim ch As New Label
+            ch.AutoSize = True
+            ch.Text = "Ch. Freq.: " & network.ChannelCenterFrequencyInKilohertz & " kHz"
+            networkView.Controls.Add(ch)
+            Dim auth As New Label
+            auth.AutoSize = True
+            auth.Text = "Authen.: " & network.SecuritySettings.NetworkAuthenticationType.ToString()
+            networkView.Controls.Add(auth)
+            Dim encr As New Label
+            encr.AutoSize = True
+            encr.Text = "Encryption: " & network.SecuritySettings.NetworkEncryptionType.ToString()
+            networkView.Controls.Add(encr)
+
+
+            Dim reconnectRow As New FlowLayoutPanel
+            reconnectRow.AutoSize = True
+            Dim reconnectBox As New CheckBox
+            reconnectBox.AutoSize = True
+            Dim reconnect As New Label
+            reconnect.Text = "Connect Automatically"
+            reconnect.AutoSize = True
+            reconnectRow.Controls.Add(reconnectBox)
+            reconnectRow.Controls.Add(reconnect)
+
+            networkView.Controls.Add(reconnectRow)
+
+            Dim secretVal As New TextBox
+            If Not network.SecuritySettings.NetworkAuthenticationType = Windows.Networking.Connectivity.NetworkAuthenticationType.None And Not network.SecuritySettings.NetworkAuthenticationType = Windows.Networking.Connectivity.NetworkAuthenticationType.Open80211 Then
+                Dim secretRow As New FlowLayoutPanel
+                secretRow.AutoSize = True
+                Dim secret As New Label
+                secret.Text = "Secret Key:"
+                secret.AutoSize = True
+                secretVal.MinimumSize = New Size(130, 25)
+                secretRow.Controls.Add(secret)
+                secretRow.Controls.Add(secretVal)
+                networkView.Controls.Add(secretRow)
+            End If
+
+
+            Dim connect As New Button
+            connect.Text = "Connect"
+            connect.MinimumSize = New Size(100, 30)
+            networkView.Controls.Add(connect)
+            connect.Anchor = AnchorStyles.None
+            connect.BackColor = Color.DimGray
+            connect.Tag = New NetworkState(network, secretVal, reconnectBox)
+            AddHandler connect.Click, AddressOf Me.connect_click
+
+            networkView.MinimumSize = New Size(250, 100)
+            networkView.BackColor = Color.FromArgb(45, 45, 45)
+            networkView.ForeColor = Color.White
+            NetworksList.Controls.Add(networkView)
+        Next
+
+
+    End Sub
+
+    Private Async Sub connect_click(sender As Object, e As EventArgs)
+        Dim connectBtn As Button = CType(sender, Button)
+        Dim networkState As NetworkState = CType(connectBtn.Tag, NetworkState)
+        Dim credential As PasswordCredential = New PasswordCredential()
+        If Not String.IsNullOrEmpty(networkState.secret.Text) Then
+            credential.Password = networkState.secret.Text
+        End If
+
+        Dim reconnect As WiFiReconnectionKind = If(networkState.autoConnect.Checked, WiFiReconnectionKind.Automatic, WiFiReconnectionKind.Manual)
+        Console.WriteLine(reconnect)
+        Await adapter.ConnectAsync(networkState.network, reconnect, credential)
+
+    End Sub
+End Class
+
+
+Public Class NetworkState
+    Public Sub New(ByRef net As WiFiAvailableNetwork, ByRef sec As TextBox, ByRef auto As CheckBox)
+        network = net
+        secret = sec
+        autoConnect = auto
+    End Sub
+    Public network As WiFiAvailableNetwork
+    Public secret As TextBox
+    Public autoConnect As CheckBox
+End Class
